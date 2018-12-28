113083c0a74f2081198c5084ff4bcff243fcc498
venkatachaitu <venkatachaitu@gmail.com>
2018-12-28 10:57:27 +0530

    key updated

--- src/main/java/com/chaitu/controller/CityOmniController.java
+++ src/main/java/com/chaitu/controller/CityOmniController.java
@@ -416,12 +416,12 @@ public class CityOmniController {
 		}
     	String uri = "";
     	if (category.equalsIgnoreCase("all")) {
-			uri = "https://maps.googleapis.com/maps/api/place/radarsearch/json?location="+lat+","+lon+"&rankby=distance&radius="+radius+"&keyword="+keyword+"&key=AIzaSyCw-Viepxab4m9pRRQyjm_yRq1uhOj9iPc";
+			uri = "https://maps.googleapis.com/maps/api/place/radarsearch/json?location="+lat+","+lon+"&rankby=distance&radius="+radius+"&keyword="+keyword+"&key=AIzaSyCQ0FD8o_1KjowT5c_c0qY6cQhvGiNwWPg";
 		}else{
 	    	if (keyword.equalsIgnoreCase("all")) {
 	    		keyword = "";
 			}
-	    	uri = "https://maps.googleapis.com/maps/api/place/radarsearch/json?location="+lat+","+lon+"&rankby=distance&radius="+radius+"&type="+category+"&keyword="+keyword+"&key=AIzaSyCw-Viepxab4m9pRRQyjm_yRq1uhOj9iPc";
+	    	uri = "https://maps.googleapis.com/maps/api/place/radarsearch/json?location="+lat+","+lon+"&rankby=distance&radius="+radius+"&type="+category+"&keyword="+keyword+"&key=AIzaSyCQ0FD8o_1KjowT5c_c0qY6cQhvGiNwWPg";
 		}
     	System.out.println("searchGoogleApi : "+uri);
 		Map<?, ?> result = restTemplate.getForObject(uri, Map.class);
@@ -433,7 +433,7 @@ public class CityOmniController {
     ///"/rest/get/search/getDetailsByPlaceId/{place_id}"
     @GetMapping("/rest/get/search/getDetailsByPlaceId/{place_id}")
     public ResponseEntity<Object> getDetailsByPlaceId(@PathVariable String place_id) throws Exception {
-    	String uri = "https://maps.googleapis.com/maps/api/place/details/json?placeid="+place_id+"&key=AIzaSyCw-Viepxab4m9pRRQyjm_yRq1uhOj9iPc";
+    	String uri = "https://maps.googleapis.com/maps/api/place/details/json?placeid="+place_id+"&key=AIzaSyCQ0FD8o_1KjowT5c_c0qY6cQhvGiNwWPg";
     	System.out.println("getDetailsByPlaceId : "+uri);
     	Map<?, ?> result = restTemplate.getForObject(uri, Map.class);
     	//System.out.println(result.get("result"));
